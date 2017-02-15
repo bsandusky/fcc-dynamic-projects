31ab41bc96a7ea3e2ae8fac4c3e773258c2c2ec2
Brett <brett.sandusky@gmail.com>
2017-02-15 21:18:26 +0000

    update index and login views

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -2,32 +2,16 @@
 
 (function () {
 
-   var addButton = document.querySelector('.btn-add');
-   var deleteButton = document.querySelector('.btn-delete');
-   var clickNbr = document.querySelector('#click-nbr');
-   var apiUrl = appUrl + '/api/:id/clicks';
+   var createNewPollButton = document.getElementById("create-new-poll-button");
+   var viewAllPollsButton = document.getElementById("view-all-polls-button");
+   // var apiUrl = appUrl + '/api/:id/clicks';
 
-   function updateClickCount (data) {
-      var clicksObject = JSON.parse(data);
-      clickNbr.innerHTML = clicksObject.clicks;
-   }
-
-   ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount));
-
-   addButton.addEventListener('click', function () {
-
-      ajaxFunctions.ajaxRequest('POST', apiUrl, function () {
-         ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount);
-      });
-
-   }, false);
-
-   deleteButton.addEventListener('click', function () {
-
-      ajaxFunctions.ajaxRequest('DELETE', apiUrl, function () {
-         ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount);
-      });
-
-   }, false);
+   createNewPollButton.addEventListener('click', function () {
+      console.log("createNewPollButton clicked");
+   });
+   
+   viewAllPollsButton.addEventListener('click', function () {
+      console.log("viewAllPollsButton clicked")
+   });
 
 })();
